209a3b66505066bbbeaad62ec9f0c83d0caa50e2
Shahan Neda <shahan.neda@gmail.com>
2020-01-24 19:35:34 -0800

    addeed buzzlist and control panel

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const server = require("http").createServer(app);// from my research we create our own http server instead of just using the one express gives so we can use the same one for socket io
+const buzzList = [];
 
 const port = 3000;
 var socket = require('socket.io').listen(server);
@@ -9,7 +10,9 @@ var socket = require('socket.io').listen(server);
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-
+app.get("/control", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/control.html"));
+});
 app.use(express.static(path.join(__dirname, 'public')))
 
 
@@ -18,6 +21,8 @@ socket.on('connect', function (io) {
     io.on('buzz', function (data) {
         console.log("Buzz!! \n " + data);
         io.broadcast.emit("buzz", data);
+        buzzList.push(data);
+        socket.emit("buzzListUpdate", { buzzList: buzzList });
     });
 
 });
