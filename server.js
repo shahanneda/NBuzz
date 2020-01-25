653472922a7fc9511d90382c35523bc2bc2a45fd
Shahan Neda <shahan.neda@gmail.com>
2020-01-24 18:32:35 -0800

    made server host static files

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.get("/", function (req, res) {
-    res.send("Test");
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.listen(port, function () {
 
     console.log("Server Running on port " + port);
