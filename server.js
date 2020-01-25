233b5beb05055d80740b13bc36020fe390ef6aa2
Shahan Neda <shahan.neda@gmail.com>
2020-01-24 19:09:28 -0800

    added socket io and setup basic socket

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const server = require("http").createServer(app);// from my research we create our own http server instead of just using the one express gives so we can use the same one for socket io
+
 const port = 3000;
+var socket = require('socket.io').listen(server);
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -9,8 +12,14 @@ app.get("/", function (req, res) {
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(port, function () {
 
-    console.log("Server Running on port " + port);
 
+socket.on('connect', function (io) {
+    io.on('event', function (data) {
+        console.log(data);
+    });
+
+    socket.emit("new", {});
 });
+socket.on('disconnect', function () { });
+server.listen(port);
